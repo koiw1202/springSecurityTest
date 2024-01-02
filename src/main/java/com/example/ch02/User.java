1eb6d69866cecf852b07c21f42617e32a196b7c0
koiw1202 <koiw1202@naver.com>
2024-01-03 06:04:36 +0900

    ch05 추가

--- src/main/java/com/example/ch02/User.java
+++ src/main/java/com/example/ch02/User.java
@@ -1,62 +1,62 @@
-package com.example.ch02;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
-
-/**
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2023-12-30        koiw1       최초 생성
- */
-public class User implements UserDetails {
-
-    private String username ;
-    private String password ;
-    private String authority ;
-
-    public User(String username, String password, String authority) {
-        this.username = username ;
-        this.password = password ;
-        this.authority = authority ;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(() -> authority);
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true ;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true ;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true ;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true ;
-    }
-}
+//package com.example.ch02;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Collection;
+//import java.util.List;
+//
+///**
+// * description    :
+// * ===========================================================
+// * DATE              AUTHOR             NOTE
+// * -----------------------------------------------------------
+// * 2023-12-30        koiw1       최초 생성
+// */
+//public class User implements UserDetails {
+//
+//    private String username ;
+//    private String password ;
+//    private String authority ;
+//
+//    public User(String username, String password, String authority) {
+//        this.username = username ;
+//        this.password = password ;
+//        this.authority = authority ;
+//    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return List.of(() -> authority);
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true ;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true ;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true ;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true ;
+//    }
+//}
--- src/main/java/com/example/ch03/PlainTextPasswordEncoder.java
+++ src/main/java/com/example/ch03/PlainTextPasswordEncoder.java
@@ -1,23 +1,23 @@
-package com.example.ch03;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-/**
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2023-12-30        koiw1       최초 생성
- */
-public class PlainTextPasswordEncoder implements PasswordEncoder {
-
-    @Override
-    public String encode(CharSequence rawPassword) {
-        return rawPassword.toString() ;
-    }
-
-    @Override
-    public boolean matches(CharSequence rawPassword, String encodedPassword) {
-        return rawPassword.equals(encodedPassword);
-    }
-}
+//package com.example.ch03;
+//
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+///**
+// * description    :
+// * ===========================================================
+// * DATE              AUTHOR             NOTE
+// * -----------------------------------------------------------
+// * 2023-12-30        koiw1       최초 생성
+// */
+//public class PlainTextPasswordEncoder implements PasswordEncoder {
+//
+//    @Override
+//    public String encode(CharSequence rawPassword) {
+//        return rawPassword.toString() ;
+//    }
+//
+//    @Override
+//    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+//        return rawPassword.equals(encodedPassword);
+//    }
+//}
