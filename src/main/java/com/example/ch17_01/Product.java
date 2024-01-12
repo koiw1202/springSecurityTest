1185eb74ca0109b62f9020737b2f04810aacb296
koiw1202 <koiw1202@naver.com>
2024-01-12 17:17:29 +0900

    ch17 사전, 사후 필터링 추가하기 연습

--- src/main/java/com/example/ch17_01/Product.java
+++ src/main/java/com/example/ch17_01/Product.java
@@ -1,22 +1,22 @@
-package com.example.ch17_01;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-/**
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2024-01-12        koiw1       최초 생성
- */
-@Getter
-@Setter
-@AllArgsConstructor
-public class Product {
-
-    private String name ;
-    private String owner ;
-
-}
+//package com.example.ch17_01;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+///**
+// * description    :
+// * ===========================================================
+// * DATE              AUTHOR             NOTE
+// * -----------------------------------------------------------
+// * 2024-01-12        koiw1       최초 생성
+// */
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//public class Product {
+//
+//    private String name ;
+//    private String owner ;
+//
+//}
--- src/main/java/com/example/ch17_01/ProductConfig.java
+++ src/main/java/com/example/ch17_01/ProductConfig.java
@@ -1,57 +1,49 @@
-package com.example.ch17_01;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-/**
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2024-01-12        koiw1       최초 생성
- */
-@Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class ProductConfig extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var manager = new InMemoryUserDetailsManager() ;
-
-        var u1 = User.withUsername("nikolai")
-                .password("12345")
-                .authorities("READ")
-                .build() ;
-
-        var u2 = User.withUsername("julien")
-                .password("12345")
-                .authorities("READ")
-                .build() ;
-
-        manager.createUser(u1);
-        manager.createUser(u2);
-
-        return manager ;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance() ;
-    }
-
-}
-
-
-
-
-
-
-
-
+//package com.example.ch17_01;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//
+///**
+// * description    :
+// * ===========================================================
+// * DATE              AUTHOR             NOTE
+// * -----------------------------------------------------------
+// * 2024-01-12        koiw1       최초 생성
+// */
+//@Configuration
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class ProductConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        var manager = new InMemoryUserDetailsManager() ;
+//
+//        var u1 = User.withUsername("nikolai")
+//                .password("12345")
+//                .authorities("READ")
+//                .build() ;
+//
+//        var u2 = User.withUsername("julien")
+//                .password("12345")
+//                .authorities("READ")
+//                .build() ;
+//
+//        manager.createUser(u1);
+//        manager.createUser(u2);
+//
+//        return manager ;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance() ;
+//    }
+//
+//}
